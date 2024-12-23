f6f35d509bb0c61f8894b31a9f571e901b7ee31c
Amogh Shetty <amoggo2004@gmail.com>
2024-12-23 17:32:05 +0300

    Updated to React 19 + Implemented addIngredients()

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,33 @@
 import { useState } from 'react'
 
 export default function Main() {
+    const [ingredients, setIngdredients] = useState([])
+
+    const ingList = ingredients.map((ing) => {
+        return (
+            <li>{ing}</li>
+        )
+    })
+
+    function addIngredient(formData) {
+        const newIngredient = formData.get("ingredient")
+        setIngdredients(prevIngredients => [...prevIngredients, newIngredient])
+    }
+
     return (
         <main>
-            <form className="add-ingredient-form">
+            <form action={addIngredient} className="add-ingredient-form">
                 <input 
                     type="text"
                     placeholder="e.g. oregano"
                     aria-label="Add ingredient"
+                    name="ingredient"
                 />
                 <button>Add ingredient</button>
             </form>
+            <ul>
+                {ingList}
+            </ul>
         </main>
     )
 }
