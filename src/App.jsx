cf0440de2eac53d5e771c06fd47a4558cc2a8b24
Amogh Shetty <amoggo2004@gmail.com>
2024-12-25 15:36:57 +0300

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header.jsx'
 import Main from './components/Main.jsx'
+import Footer from './components/Footer.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
     <>
       <Header />
       <Main />
+      <Footer />
     </>
   )
 }
